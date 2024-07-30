4f7c6db3085326896648bbb1a550c3ba933c178b
ogamanbs <134797606+ogamanbs@users.noreply.github.com>
2024-07-30 22:04:24 +0530

    added the tailwind css for react

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+const tailwindConfig = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+export default tailwindConfig;
