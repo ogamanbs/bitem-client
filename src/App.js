878db702b251fa958c92d96bdc99569ab3999978
ogamanbs <134797606+ogamanbs@users.noreply.github.com>
2024-08-26 10:32:17 +0530

    when product will be added then the cache will be updated now

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {Routes, Route, Navigate} from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Shop from './Pages/Shop/Shop';
@@ -6,7 +6,22 @@ import { useCookies } from 'react-cookie';
 import Profile from './Pages/Profile/Profile';
 import ProtectedRoutes from './Pages/ProtectedRoutes';
 
-export default function App () {
+const getProducts = async () => {
+    try {
+        const response = await fetch('https://server.bitem.in/products/all');
+        if(response.ok) {
+            const data = await response.json();
+            return data;
+        } else {
+            const data = await response.json();
+            return data;
+        }
+    } catch(err) {
+        return {message: 'error fetching products', products: null};
+    }
+}
+
+export default function App() {
     const [user, setUser] = useState(() => {
         const savedUser = localStorage.getItem('user');
         return savedUser !== null ? JSON.parse(savedUser) : null;
@@ -17,6 +32,23 @@ export default function App () {
     });
     const [, setMessage] = useState("");
     const [cookies] = useCookies(['token']);
+
+    useEffect(()=>{
+        const callAPI = async () => {
+            const data_products = await getProducts();
+            let cachedProducts = localStorage.getItem('products');
+            cachedProducts = JSON.parse(cachedProducts);
+            if(data_products.products) {
+                if(data_products.products.length !== cachedProducts.length) {
+                    localStorage.setItem('products', JSON.stringify(data_products.products));
+                    setProducts(data_products.products);
+                }
+            }
+        }
+        if(cookies.token) {
+            callAPI();
+        }
+    }, [cookies]);
 
     return(
         <div className="">
