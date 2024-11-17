6902319eb5171df6d11621ad9757551e6b1b1c81
ogamanbs <134797606+ogamanbs@users.noreply.github.com>
2024-11-17 07:51:18 +0530

    fixed the small images bug on the products page for the landscape view

--- src/Components/ProductPageComponents/SmallImages.js
+++ src/Components/ProductPageComponents/SmallImages.js
@@ -5,7 +5,7 @@ export default function SmallImages({images, name, imgnum, handleClick}) {
         <>
         {
             images.map((image, index) => (
-                <div key={index} className={`w-1/4 h-[9vh] p-1 border ${imgnum === index ? "border-blue-500" : "border-zinc-500"} cursor-pointer`} onMouseOver={() => {handleClick(index)}}>
+                <div key={index} className={`w-1/4 md:w-full h-[9vh] p-1 border ${imgnum === index ? "border-blue-500" : "border-zinc-500"} cursor-pointer`} onMouseOver={() => {handleClick(index)}}>
                     <img className="h-full w-full object-contain" src={image} alt={name}/>
                 </div>
             ))
